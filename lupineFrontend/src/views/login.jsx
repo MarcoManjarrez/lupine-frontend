8f5ee6691b53ca1ceb3f14095918b3b75cdd5094
MarcoManjarrez <142528062+MarcoManjarrez@users.noreply.github.com>
2025-05-16 23:31:37 -0600

    Login animations

--- lupineFrontend/src/views/login.jsx
+++ lupineFrontend/src/views/login.jsx
@@ -7,15 +7,18 @@ const Login = () => {
   return (
     <>
       <div className='loginContainer'>
-        <img src={lupineLogo} alt="logo"/>
-        <h1>¿Ya tienes cuenta?</h1>
-       
-        <div className='loginContainer__userPassContainer'>
-          <h2>Inicia Sesion</h2>
-          <input placeholder='Usuario'/>
-          <input placeholder='Contraseña'/>
+        <div className='loginContainer__logoHolder'>
+          <img src={lupineLogo} alt="logo"/>
+          <h1>¿Ya tienes cuenta?</h1>
         </div>
-        <div>
+        <div className='loginContainer__userPassAnimation'>
+          <div className='loginContainer__userPassContainer'>
+            <h2>Inicia Sesion</h2>
+            <input placeholder='Usuario'/>
+            <input placeholder='Contraseña'/>
+        </div>
+        </div>
+        <div className='loginContainer__registerContainer'>
           <h5>O registrate</h5>
           <button>Registrarse</button>
         </div>
